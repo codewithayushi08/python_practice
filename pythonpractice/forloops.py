0c5dc73087ca2e49f5015922a98de71afb4ac93d
codewithayushi08 <ayushidas20072@gmail.com>
2026-04-30 15:22:39 +0530

    Update forloops.py

--- pythonpractice/forloops.py
+++ pythonpractice/forloops.py
@@ -1,107 +1,121 @@
-nums=[1,2,3,4,5]
 
+
+
+
+## DAY 6- for loops and range
+
+# ----------------------
+# BASIC FOR LOOPS
+# ----------------------
+
+nums = [1, 2, 3, 4, 5]
 for val in nums:
     print(val)
 
-
-veggies=["potatoes","brinjal","ladyfinger","cucumber"]
+veggies = ["potatoes", "brinjal", "ladyfinger", "cucumber"]
 for val in veggies:
     print(val)
 
-
-tup=[1,2,3,4,2,8,9] 
+tup = (1, 2, 3, 4, 2, 8, 9)
 for num in tup:
-    print(num)   
+    print(num)
 
-str="sai"
-for char in str:
-    if(char=="a"):
+# ----------------------
+# STRING LOOP
+# ----------------------
+
+text = "sai"
+for char in text:
+    if char == "a":
         print("a found")
         break
     print(char)
 else:
     print("end")
 
-#practice questions
-#print the elements of the following list using a loop
-nums=[1,4,9,16,25,36,49,64,81,100]
+# ----------------------
+# PRACTICE: PRINT LIST
+# ----------------------
+
+nums = [1,4,9,16,25,36,49,64,81,100]
 for val in nums:
     print(val)
 
+# ----------------------
+# SEARCH IN TUPLE
+# ----------------------
 
+tup = (1,4,9,16,25,36,49,64,81,100)
+x = 16
 
-
-#search for a number x in this tuple using loop:
-tup=[1,4,9,16,25,36,49,64,81,100]
-x=16
-i=0
-for num in tup:
-    if(num==x):
-        print("found at idx",i)
+for idx, num in enumerate(tup):
+    if num == x:
+        print("Found at index:", idx)
         break
-    i+=1
-    print(num)
 else:
-    print("not found")
+    print("Not found")
 
+# ----------------------
+# RANGE
+# ----------------------
 
-#RANGE
-seq=range(10)
-for i in seq:
+for i in range(10):
     print(i)
-print(seq[1])
-print(seq[2])
-print(seq[3])
+
 print(range(5))
 
-
-for i in range(1,100,2):
+for i in range(1, 100, 2):
     print(i)
 
+# ----------------------
+# NUMBER PRINTING
+# ----------------------
 
-#using for and range
-#print numbers from 1 to 100
-for i in range(1,101):
+# 1 to 100
+for i in range(1, 101):
     print(i)
 
-
-#print numbers from 100 to 1
-for i in range (100,0,-1):
+# 100 to 1
+for i in range(100, 0, -1):
     print(i)
 
+# ----------------------
+# MULTIPLICATION TABLE
+# ----------------------
 
-#print the multiplication table of a  number n.
+n = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(n * i)
 
-n=int(input("enter a number:"))
-for i in range(1,11):
-    print(n*i)
-    
+# ----------------------
+# PASS STATEMENT
+# ----------------------
 
-#pass statements
-for i in range (5):
+for i in range(5):
     pass
 
+print("Some useful work")
 
-print("some useful work")
+# ----------------------
+# SUM OF FIRST N NUMBERS
+# ----------------------
 
+n = 5
+total = 0
 
+for i in range(1, n + 1):
+    total += i
 
-#WAP TO FIND SUM OF FIRST N NUMBERS (USING WHILE LOOP)
-n=5
+print("Total sum =", total)
 
-sum=0
-for i in range(1,n+1):
-    print(i)
-    sum+=i
-print("total sum=",sum)
+# ----------------------
+# FACTORIAL
+# ----------------------
 
+n = int(input("Enter a number: "))
+fact = 1
 
+for i in range(1, n + 1):
+    fact *= i
 
-#wap to find the factorial of first n numbers.(using for)
-n=int(input("enter a number:"))
-fact=1
-i=1
-for i in range(1,n+1):
-    
-    fact*=i
-print("the final factorial=",fact)
+print("Factorial =", fact)
