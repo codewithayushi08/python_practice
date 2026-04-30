dd290a8a0074e539fcb77247d231931af3b2a402
codewithayushi08 <ayushidas20072@gmail.com>
2026-04-30 15:16:30 +0530

    Update whileloops.py

--- pythonpractice/whileloops.py
+++ pythonpractice/whileloops.py
@@ -1,102 +1,104 @@
-#count=1
-#while count<=5:
-   # print("hello")
-  #  count +=1
-#i=1
-##while i<=100000:
- #   print("sonakshi",i)
-  #  i +=1
+# Day 5 - While Loops
 
+# ----------------------
+# BASIC LOOPS
+# ----------------------
 
-#print numbers from 1 to 5
-
-i=5
-while i>=1:
+# Print numbers from 5 to 1
+i = 5
+while i >= 1:
     print(i)
-    i-=1
-print("loop ended")
+    i -= 1
+print("Loop ended")
 
+# Print numbers from 1 to 100
+num = 1
+while num <= 100:
+    print(num)
+    num += 1
 
-#print numbers from 1 to 100
-a=1
-while a<=100:
-    print(a)
-    a+=1
+# Print numbers from 100 to 1
+num = 100
+while num >= 1:
+    print(num)
+    num -= 1
+print("Loop ended")
 
+# ----------------------
+# MULTIPLICATION TABLE
+# ----------------------
 
-#print numbers from 100 to 1
-b=100
-while b>=1:
-    print(b)
-    b-=1
-print("loop ended")
+n = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(n * i)
+    i += 1
 
+# ----------------------
+# LIST USING LOOP
+# ----------------------
 
-#print the multiplication table of number n
-n=int(input("enter a number:"))
-i=1
-while i<=10:
-    print(n*i)
-    i+=1
-print("loop ended")
+nums = [1,4,9,16,25,36,49,64,81,100]
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
 
+# ----------------------
+# TUPLE LOOP
+# ----------------------
 
-#print the elements of the following list using a loop:
-[1,4,9,16,25,36,49,64,81,100]
-s=1
-while s<=10:
-    print(s*s)
-    s+=1
-print("squares")
+heroes = ("ironman", "thor", "superman", "batman")
+i = 0
+while i < len(heroes):
+    print(heroes[i])
+    i += 1
 
+# ----------------------
+# SEARCH IN TUPLE
+# ----------------------
 
-#search for a number x in this tuple using loop:
-nums=(1,4,9,16,25,36,49,64,81,100)
-heroes=("ironman","thor","superman","batsman")
-idx=0
-while idx<len(heroes):
-    print(heroes[idx])  #nums[0],nums[1],nums[2]....
-    idx+=1
-    
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = 36
+i = 0
 
-#tuple search x
-nums=(1,4,9,16,25,36,49,64,81,100)
-x=36
-i=0
-while i< len(nums):
-    if(nums[i]==x):
-        print("FOUND at idx ",i)
+while i < len(nums):
+    if nums[i] == x:
+        print("Found at index:", i)
         break
-    else:
-        print("finding..")
-    i+=1
-print("end of loop")
-# break and continue
-i=1
-while i<=5:
+    i += 1
+
+print("End of loop")
+
+# ----------------------
+# BREAK EXAMPLE
+# ----------------------
+
+i = 1
+while i <= 5:
     print(i)
-    if(i==3):
+    if i == 3:
         break
-    i+=1
-print("end of loop")    
-#continue
+    i += 1
 
+print("End of loop")
 
-i=0
-while i<=5:
-    if(i==3):
-        i+=1
-        continue  #skip
-    print(i)
-    i+=1
+# ----------------------
+# CONTINUE EXAMPLE
+# ----------------------
 
-i=1
-while i<=10:
-    if( i % 2 !=0):
-        i+=1
+i = 0
+while i <= 5:
+    i += 1
+    if i == 3:
         continue
-    print(i) 
-    i+=1
+    print(i)
 
-p
-
+# Print even numbers
+i = 1
+while i <= 10:
+    if i % 2 != 0:
+        i += 1
+        continue
+    print(i)
+    i += 1
