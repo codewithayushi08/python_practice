187e4e2702115aecfc99efc5c1fe3030e95c686b
codewithayushi08 <ayushidas20072@gmail.com>
2026-05-03 21:55:35 +0530

    updated palindrome and number programs

--- pythonpractice/practice.py
+++ pythonpractice/practice.py
@@ -20,10 +20,47 @@ if(d%7==0):
    print("multiple of 7")
 else:
    print("not multiple of 7")
+
 #WAP TO FIND SMALLEST OF 3 NUMBERS
+num1=int(input("Enter num1="))
+num2=int(input("Enter num2="))
+num3=int(input("Enter num3="))
+smallest=num1
+
+if num2<smallest:
+   smallest=num2
+
+if num3<smallest:
+   smallest=num3
+
+print("smallest number is:",smallest)
+
 #WAP TO CHECK LEAP YEAR
+def is_leap_year(year):
+   if(year % 4 == 0 and year % 100!=0) or (year %400==0):
+    return True
+   else:
+      return False
+year=int(input("Enter year:"))
+print(is_leap_year(year))
+
 #WAP TO CHECK POSITIVE/NEGATIVE/ZERO
+number=int(input("Enter a number:"))
+if number>0:
+   print("positive number")
+elif number<0:
+   print("negative number")
+else:
+   print("zero")
+
+
+
 #WAP TO PRINT NUMBERS FROM 1 TO N
+n=int(input("Enter value of n:"))
+for i in range(1,n+1):
+   print(i)
+
+
 #WAP TO FIND FACTORIAL OF A NUMBER
 #WAP TO CHECK PALINDROME NUMBER
 
